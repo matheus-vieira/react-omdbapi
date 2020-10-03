6cca96adc2f5b99849e163d42c7e46fe967ab618
Matheus Costa Vieira <matheus.costa.vieira@gmail.com>
2020-10-03 19:10:21 -0300

    added a load more method, saving the search url

--- react-omdbapi/src/service/media.js
+++ react-omdbapi/src/service/media.js
@@ -1,15 +1,23 @@
 const url = "http://www.omdbapi.com/?apikey=17cb3543";
+let searchUrl = "";
 
-export const search = (searchTerm, type = "series", page = 1) => {
-    const fetchUrl = `${url}&s=${searchTerm}&type=${type}&page=${page}`;
-    return fetch(fetchUrl)
-      .then((r) => r.json())
-      .catch(console.error);
-}
+export const search = (searchTerm, type = "series") => {
+  searchUrl = `${url}&s=${searchTerm}&type=${type}`;
+  return fetch(searchUrl)
+    .then((r) => r.json())
+    .catch(console.error);
+};
+
+export const loadMore = (page = 1) => {
+  const loadMoreUrl = `${searchUrl}&page=${page}`;
+  return fetch(loadMoreUrl)
+    .then((r) => r.json())
+    .catch(console.error);
+};
 
 export const get = (imdbId) => {
-    const fetchUrl = `${url}&i=${imdbId}`;
-    return fetch(fetchUrl)
-      .then((r) => r.json())
-      .catch(console.error);
-}
+  const fetchUrl = `${url}&i=${imdbId}`;
+  return fetch(fetchUrl)
+    .then((r) => r.json())
+    .catch(console.error);
+};
